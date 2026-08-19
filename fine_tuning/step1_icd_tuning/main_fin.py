759bbdf94f8ad28c996929ec2175cbefcc586061
LAXMINARAYAN24 <sahulucky2411@gmail.com>
2026-08-19 23:31:46 +0000

    fine tunned

--- fine_tuning/step1_icd_tuning/main_fin.py
+++ fine_tuning/step1_icd_tuning/main_fin.py
@@ -122,13 +122,6 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--save_steps",
-        type=int,
-        default=100,
-        help="Save checkpoint every N steps",
-    )
-
-    parser.add_argument(
         "--output_path",
         type=str,
         default="output",
